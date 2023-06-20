2a6f7988c75d7c55e686a9b77e231eab775a07da
Yunique <qq643374843@163.com>
2023-06-20 16:11:42 +0800

    Add AboutManager for produce the About content.

---
+++ DiffViewer/Managers/AboutManager.cs
@@ -0,0 +1,217 @@
+﻿using DiffViewer.Managers.Helper;
+using DiffViewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace DiffViewer.Managers;
+
+internal class AboutManager
+{
+
+    #region FlowDocumentContent
+
+    private string _softwareName = nameof(_softwareName);
+    private string _softwareVersion = nameof(_softwareVersion);
+    private string _copyrightOwner = nameof(_copyrightOwner);
+    private string _releaseDate = nameof(_releaseDate);
+
+    private List<OpenSourceProject> _openSourceLibraries = new List<OpenSourceProject>
+    {
+        new OpenSourceProject { Name = "Microsoft.Xaml.Behaviors.Wpf", Version = "1.1.39", License = "MIT", Url = "https://github.com/Microsoft/XamlBehaviorsWpf" },
+        new OpenSourceProject { Name = "MiniExcel", Version = "1.30.3", License = "Apache-2.0", Url = "https://github.com/mini-software/MiniExcel" },
+        new OpenSourceProject { Name = "Newtonsoft.Json", Version = "13.0.3", License = "MIT", Url = "https://www.newtonsoft.com/json" }
+    };
+
+    private List<OpenSourceProject> _thirdPartyComponents = new List<OpenSourceProject>
+    {
+        new OpenSourceProject { Name = "ICON", Version = "", License = "", Url = "https://icons8.com/" },
+        //new OpenSourceProject { Name = "第三方组件名称2", Version = "第三方组件版本号2", License = "第三方组件许可证2", Url = "http://Bing.com" },
+        //new OpenSourceProject { Name = "第三方组件名称3", Version = "第三方组件版本号3", License = "第三方组件许可证3", Url = "http://Google.com" }
+    };
+
+    private List<OpenSourceProject> _specialThanks = new List<OpenSourceProject>
+    {
+            new OpenSourceProject { Name = "HYSYS", Version = "", License = "", Url = "https://www.aspentech.com/en/products/engineering/aspen-hysys/?src=web-apaccn" },
+            new OpenSourceProject { Name = "AspenTech", Version = "", License = "", Url = "https://www.aspentech.cn/" },
+    };
+
+    public string SoftwareName
+    {
+        get => _softwareName;
+        set => _softwareName = value;
+    }
+
+    public string SoftwareVersion
+    {
+        get => _softwareVersion;
+        set => _softwareVersion = value;
+    }
+
+    public string CopyrightOwner
+    {
+        get => _copyrightOwner;
+        set => _copyrightOwner = value;
+    }
+
+    public string ReleaseDate
+    {
+        get => _releaseDate;
+        set => _releaseDate = value;
+    }
+
+    public List<OpenSourceProject> OpenSourceLibraries
+    {
+        get => _openSourceLibraries;
+        set => _openSourceLibraries = value;
+    }
+
+    public List<OpenSourceProject> ThirdPartyComponents
+    {
+        get => _thirdPartyComponents;
+        set => _thirdPartyComponents = value;
+    }
+
+    public List<OpenSourceProject> SpecialThanks
+    {
+        get => _specialThanks;
+        set => _specialThanks = value;
+    }
+
+    public FlowDocument GenerateLicenseInfoDocument()
+    {
+        FlowDocument flowDocument = new FlowDocument();
+        AddParagraph(flowDocument, new Bold(new Run($"{SoftwareName} v{SoftwareVersion}")));
+        AddParagraph(flowDocument, new Run($"{GetStringFromAppResources("CopyrightOwner")} © {CopyrightOwner} {ReleaseDate}"));
+        AddParagraph(flowDocument, new Run($"{GetStringFromAppResources("ThirdLibraries")}"));
+        AddList(flowDocument, OpenSourceLibraries);
+        AddParagraph(flowDocument, new Run($"{GetStringFromAppResources("ThirdComponents")}"));
+        AddList(flowDocument, ThirdPartyComponents);
+        AddParagraph(flowDocument, new Run($"{GetStringFromAppResources("SpecialThanks")}"));
+        AddList(flowDocument, SpecialThanks);
+        AddExtraInfosList(flowDocument, null);
+
+        return flowDocument;
+    }
+
+    private void AddParagraph(FlowDocument flowDocument, Inline inline)
+    {
+        Paragraph paragraph = new Paragraph();
+        paragraph.Inlines.Add(inline);
+        flowDocument.Blocks.Add(paragraph);
+    }
+
+    private void AddList(FlowDocument flowDocument, List<OpenSourceProject> items)
+    {
+        List list = new List();
+        foreach (OpenSourceProject item in items)
+        {
+            ListItem listItem = new ListItem();
+
+            if (!string.IsNullOrEmpty(item.Url))
+            {
+                Hyperlink hyperlink = new Hyperlink(new Run($"{item.Name} " + (item.Version.IsNullOrWhiteSpaceOrEmpty() ? "" : $"v{item.Version}")));
+                hyperlink.NavigateUri = new Uri(item.Url); // set the hyperlink target
+                hyperlink.RequestNavigate += Hyperlink_RequestNavigate; // add event handler
+                listItem.Blocks.Add(new Paragraph(hyperlink));
+            }
+            else
+            {
+                listItem.Blocks.Add(new Paragraph(new Run($"{item.Name} " + (item.Version.IsNullOrWhiteSpaceOrEmpty() ? "" : $"v{item.Version}"))));
+            }
+
+            if (!string.IsNullOrEmpty(item.License))
+            {
+                listItem.Blocks.Add(new Paragraph(new Italic(new Run($"{GetStringFromAppResources("License")} {item.License}"))));
+            }
+
+            list.ListItems.Add(listItem);
+        }
+        flowDocument.Blocks.Add(list);
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        try
+        {
+            // open the link in the default browser
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            };
+
+            Process.Start(startInfo);
+        }
+        catch (Exception ex)
+        {
+            // handle the exception
+        }
+
+        e.Handled = true;
+    }
+
+    //TODO: Add update/usage/etc infos
+    private void AddExtraInfosList(FlowDocument flowDocument, List<OpenSourceProject> items)
+    {
+        AddParagraph(flowDocument, new Run($"{GetStringFromAppResources("Slogan")}"));
+    }
+
+    #endregion FlowDocumentContent
+
+    private void GetSoftwareAssemblyInfos()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        AssemblyName assemblyName = assembly.GetName();
+
+        string name = assemblyName.Name ?? "";
+        string version = (assemblyName.Version ?? new Version("0.0.0.0")).ToString();
+        string copyright = string.Empty;
+        string releaseDate = string.Empty;
+
+        object[] attributes = assembly.GetCustomAttributes(typeof(AssemblyTrademarkAttribute), false);
+        if (attributes.Length > 0)
+        {
+            AssemblyTrademarkAttribute trademarkAttribute = (AssemblyTrademarkAttribute)attributes[0];
+            copyright = trademarkAttribute.Trademark;
+        }
+
+        attributes = assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false);
+        if (attributes.Length > 0)
+        {
+            AssemblyInformationalVersionAttribute informationalVersionAttribute = (AssemblyInformationalVersionAttribute)attributes[0];
+            releaseDate = informationalVersionAttribute.InformationalVersion.Split('+')[0];
+        }
+
+        _softwareName = name;
+        _softwareVersion = version;
+        _copyrightOwner = copyright;
+        _releaseDate = releaseDate;
+    }
+
+    private string GetStringFromAppResources(string resourcekey)
+    {
+        string location = $"({nameof(AboutManager)}).({nameof(GetStringFromAppResources)})";
+        try
+        {
+            string value = App.Current.Resources.MergedDictionaries[0][resourcekey].ToString() ?? "-".Repeat(6);
+            location += $". Value found: {value}.";
+            App.Logger.Information(location);
+            return value;
+        }
+        catch (Exception e)
+        {
+            location += " cause problem.";
+            App.Logger.Error(location, e);
+            throw;
+        }
+    }
+
+}
