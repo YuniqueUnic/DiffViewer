0ea8bf369d0c2f3a4904d74b1b2bdf07bbac884b
Yogi-DeskTop <643374843@qq.com>
2023-06-29 00:36:44 +0800

    Add search funtion for raw window

--- DiffViewer/Views/RawDataWindow.xaml.cs
+++ DiffViewer/Views/RawDataWindow.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
-using DiffViewer.Managers;
 using DiffViewer.Messages;
-using DiffViewer.Models;
 using MvvmDialogs;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace DiffViewer.Views;
 
@@ -24,7 +27,6 @@ public partial class RawDataWindow : Window, IWindow
 
     ContentControl IWindow.Owner { get => this.Owner; set => this.Owner = (Window)value; }
 
-
     /// <summary>
     /// Registe all messengers.
     /// </summary>
@@ -33,9 +35,9 @@ public partial class RawDataWindow : Window, IWindow
         // Set the RichText Content
         WeakReferenceMessenger.Default.Register<SetRichTextBoxDocumentMessage>(this , (o , m) =>
         {
-            if( m is null || m.Message != "LoadRawContent" || m.ObjReplied is not DiffTestCase tc ) return;
+            if( m is null || m.Message != "LoadRawContent" || m.ObjReplied is not Models.DiffTestCase tc ) return;
             this.DataContext = tc;
-            RawRichFlowDocumentReader.Document = FlowDocumentManager.CreateFlowDocument(tc.Raw ?? string.Empty);
+            //RawRichFlowDocumentReader.Document = FlowDocumentManager.CreateFlowDocument(tc.Raw ?? string.Empty);
             App.Logger.Information<SetRichTextBoxDocumentMessage>($"Show {tc.Name} Raw data window Done" , m);
         });
     }
@@ -45,6 +47,144 @@ public partial class RawDataWindow : Window, IWindow
         //base.OnClosing(e);
         this.Visibility = Visibility.Hidden;
         e.Cancel = true;
+    }
+
+    public static int currentSearchIndex = 0;
+    public static List<int> AllSearchTextStartIndexes;
+    public static int selectionLength = 0;
+
+    // 定义一个函数来搜索文本块中的字符串
+    private async void SearchButton_Click(object sender , RoutedEventArgs e)
+    {
+        // 获取 TextBox 中指定字符串的位置
+        if( string.IsNullOrEmpty(SearchTextBox.Text) || string.IsNullOrEmpty(RawTextBox.Text) ) { return; }
+        AllSearchTextStartIndexes = await SearchTextByAsync(RawTextBox.Text , SearchTextBox.Text);
+        currentSearchIndex = 0;
+        selectionLength = SearchTextBox.Text.Length;
+        NextSearchButton_Click(sender , e);
+    }
+
+    private void LastSearchButton_Click(object sender , RoutedEventArgs e)
+    {
+        if( AllSearchTextStartIndexes is not null && AllSearchTextStartIndexes.Count > 0 )
+        {
+            currentSearchIndex = Math.Max(0 , Math.Min(currentSearchIndex , AllSearchTextStartIndexes.Count - 1));
+
+            GoToTextLine(RawTextBox , AllSearchTextStartIndexes[currentSearchIndex] , selectionLength);
+
+            currentSearchIndex--;
+        }
+    }
+
+    private void NextSearchButton_Click(object sender , RoutedEventArgs e)
+    {
+        if( AllSearchTextStartIndexes is not null && AllSearchTextStartIndexes.Count > 0 )
+        {
+            currentSearchIndex = Math.Max(0 , Math.Min(currentSearchIndex , AllSearchTextStartIndexes.Count - 1));
+
+            GoToTextLine(RawTextBox , AllSearchTextStartIndexes[currentSearchIndex] , selectionLength);
+
+            currentSearchIndex++;
+        }
+    }
+
+    private void GoToTextLine(TextBox textBox , int textStartindex , int selectionlength)
+    {
+        if( textStartindex < 0 ) { return; }
+
+        // 获取包含字符串的行号和列号
+        int lineNumber = textBox.GetLineIndexFromCharacterIndex(textStartindex);
+        int columnNumber = textStartindex - textBox.GetCharacterIndexFromLineIndex(lineNumber);
+
+        // 将 TextBox 滚动到包含字符串的行
+        textBox.ScrollToLine(lineNumber);
+
+        // 将 TextBox 滚动到包含字符串的列
+        double charWidth = new FormattedText(" " ,
+            System.Globalization.CultureInfo.CurrentCulture ,
+            FlowDirection.LeftToRight ,
+            new Typeface(textBox.FontFamily ,
+                                 textBox.FontStyle ,
+                                 textBox.FontWeight ,
+                                 textBox.FontStretch) ,
+            textBox.FontSize ,
+            Brushes.Black).Width;
+
+        double offset = columnNumber * charWidth;
+
+        textBox.SelectionBrush = Brushes.Cyan;
+        textBox.Select(textStartindex , selectionlength);
+        textBox.SelectionOpacity = 0.5;
+        textBox.ScrollToHorizontalOffset(offset);
+    }
+
+    public async Task<List<int>> SearchTextByAsync(string originalText , string searchingText , int num_Segments = 4)
+    {
+        // Divide the text into segments for concurrent processing
+        //int num_Segments = 4; // Number of threads to use
+        int segment_size = originalText.Length / num_Segments; // Size of each segment
+        string[] segments = new string[num_Segments]; // String for each segment
+        int[] segment_starts = new int[num_Segments]; // Starting index for each segment
+        string buffer = ""; // String buffer to hold the leftover text from the previous segment
+
+        for( int i = 0; i < num_Segments; i++ )
+        {
+            int segment_end;
+            if( i < num_Segments - 1 )
+            {
+                segment_starts[i] = i * segment_size;
+                segment_end = segment_starts[i] + segment_size;
+            }
+            else
+            {
+                segment_starts[num_Segments - 1] = originalText.Length - (num_Segments - 1) * segment_size;
+                segment_end = originalText.Length;
+            }
+
+            if( i > 0 )
+            {
+                // Add the leftover text from the previous segment to the beginning of the current segment
+                int buffer_index = buffer.LastIndexOf('\n') + 1;
+                segments[i] = buffer.Substring(buffer_index) + originalText.Substring(segment_starts[i] , segment_end - segment_starts[i]);
+                buffer = buffer.Substring(0 , buffer_index);
+            }
+            else
+            {
+                segments[i] = originalText.Substring(segment_starts[i] , segment_end - segment_starts[i]);
+            }
+            if( i < num_Segments - 1 )
+            {
+                // Save the leftover text from the end of the current segment to the buffer
+                int buffer_index = segments[i].LastIndexOf('\n') + 1;
+                buffer += segments[i].Substring(buffer_index);
+                segments[i] = segments[i].Substring(0 , buffer_index);
+            }
+        }
+
+        // Use Task.WhenAll to search for the string concurrently
+        List<int>[] results = await Task.WhenAll(
+            Enumerable.Range(0 , num_Segments)
+                .Select(async i =>
+                {
+                    List<int> segment_results = new List<int>();
+                    int segment_start = segment_starts[i];
+                    int segment_end = i == num_Segments - 1 ? originalText.Length : segment_starts[i + 1];
+                    int result_index = segments[i].IndexOf(searchingText);
+                    while( result_index != -1 )
+                    {
+                        segment_results.Add(segment_start + result_index);
+                        result_index = segments[i].IndexOf(searchingText , result_index + 1);
+                    }
+                    await Task.Delay(0); // add this line to avoid the warning
+                    return segment_results;
+                })
+        );
+
+        // Merge the results from each segment
+        List<int> all_results = new List<int>();
+        for( int i = 0; i < num_Segments; i++ ) { all_results.AddRange(results[i]); }
+
+        return all_results;
     }
 
     private void RawTextBox_PreviewMouseWheel(object sender , System.Windows.Input.MouseWheelEventArgs e)
@@ -64,22 +204,21 @@ public partial class RawDataWindow : Window, IWindow
         }
     }
 
-    private void RawRichFlowDocumentReader_PreviewMouseWheel(object sender , System.Windows.Input.MouseWheelEventArgs e)
-    {
-        if( Keyboard.Modifiers == ModifierKeys.Control )
-        {
-            if( e.Delta > 0 )
-            {
-                RawRichFlowDocumentReader.IncreaseZoom();
-            }
-            else
-            {
-                RawRichFlowDocumentReader.DecreaseZoom();
-            }
+    //private void RawRichFlowDocumentReader_PreviewMouseWheel(object sender , System.Windows.Input.MouseWheelEventArgs e)
+    //{
+    //    if( Keyboard.Modifiers == ModifierKeys.Control )
+    //    {
+    //        if( e.Delta > 0 )
+    //        {
+    //            RawRichFlowDocumentReader.IncreaseZoom();
+    //        }
+    //        else
+    //        {
+    //            RawRichFlowDocumentReader.DecreaseZoom();
+    //        }
 
-            e.Handled = true;
-        }
-    }
-
+    //        e.Handled = true;
+    //    }
+    //}
 
 }
